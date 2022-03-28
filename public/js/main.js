7a6bed20e094e6374ac7ee762986a29524795441
Script Raccoon <scriptraccoon@gmail.com>
2022-03-29 00:32:12 +0200

    areas to control both angles at once

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,9 @@
-let angle = 0;
+// let angle = 0;
 
-function loop() {
-    document.body.style.setProperty("--angle", angle);
-    angle += Math.PI / (60 * 10);
-    requestAnimationFrame(loop);
-}
+// function loop() {
+//     document.body.style.setProperty("--angle", angle);
+//     angle += Math.PI / (60 * 10);
+//     requestAnimationFrame(loop);
+// }
 
-loop();
+// loop();
