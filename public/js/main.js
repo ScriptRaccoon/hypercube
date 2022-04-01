3dd02acc1a0ce3bdf1d417f1cce8462f095ebe72
Script Raccoon <scriptraccoon@gmail.com>
2022-04-01 21:45:44 +0200

    cleanup

--- public/js/main.js
+++
@@ -1,9 +0,0 @@
-// let angle = 0;
-
-// function loop() {
-//     document.body.style.setProperty("--angle", angle);
-//     angle += Math.PI / (60 * 10);
-//     requestAnimationFrame(loop);
-// }
-
-// loop();
